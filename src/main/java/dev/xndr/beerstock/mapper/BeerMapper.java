fe3640cf0973c4c53c9aec5a3bda10bf5946669b
Alexander <alex_osilva@hotmail.com>
2021-08-01 05:09:52 -0300

    (feat): add beer mapper

--- src/main/java/dev/xndr/beerstock/mapper/BeerMapper.java
+++ src/main/java/dev/xndr/beerstock/mapper/BeerMapper.java
@@ -1,2 +1,17 @@
-package dev.xndr.beerstock.mapper;public class BeerMapper {
+package dev.xndr.beerstock.mapper;
+
+
+import dev.xndr.beerstock.dto.BeerDTO;
+import dev.xndr.beerstock.entities.Beer;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerMapper INSTANCE = Mappers.getMapper(BeerMapper.class);
+
+    Beer toModel(BeerDTO beerDTO);
+
+    BeerDTO toDTO(Beer beer);
 }
